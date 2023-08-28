67751cc2f4e117c87ed7ac496e7a9e44618ac2c9
viKii <vikii@viKiis.local>
2023-08-28 18:55:01 +0530

    adding timestamp

--- src/screen/notification.tsx
+++ src/screen/notification.tsx
@@ -8,7 +8,34 @@ interface Notification {
     message: string;
     timestamp: string;
 }
+function calculateTimeDifference(timestamp: string): string {
+    const now = new Date();
+    const targetTime = new Date(timestamp);
 
+    const timeDifference = now.getTime() - targetTime.getTime();
+    const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+
+    if (minutesDifference < 2) {
+        return 'Just now';
+    } else if (minutesDifference < 60) {
+        return `${minutesDifference} minute${minutesDifference > 1 ? 's' : ''} ago`;
+    } else if (minutesDifference < 1440) { // 1440 minutes in a day
+        const hoursDifference = Math.floor(minutesDifference / 60);
+        return `${hoursDifference} hour${hoursDifference > 1 ? 's' : ''} ago`;
+    } else if (minutesDifference < 10080) { // 10080 minutes in a week
+        const daysDifference = Math.floor(minutesDifference / 1440);
+        return `${daysDifference} day${daysDifference > 1 ? 's' : ''} ago`;
+    } else if (minutesDifference < 43200) { // 302400 minutes in a month (approx. 30 days)
+        const weeksDifference = Math.floor(minutesDifference / 10080);
+        return `${weeksDifference} week${weeksDifference > 1 ? 's' : ''} ago`;
+    } else if (minutesDifference < 525600) { // 525600 minutes in a year
+        const monthsDifference = Math.floor(minutesDifference / 43200);
+        return `${monthsDifference} month${monthsDifference > 1 ? 's' : ''} ago`;
+    } else {
+        const yearsDifference = Math.floor(minutesDifference / 525600);
+        return `${yearsDifference} year${yearsDifference > 1 ? 's' : ''} ago`;
+    }
+}
 const NotificationsScreen: React.FC = () => {
     const [notificationData, setNotificationData] = useState<Notification[]>([]);
 
@@ -39,7 +66,7 @@ const NotificationsScreen: React.FC = () => {
                             <h3>{notification.title}</h3>
                             <p>{notification.message}</p>
                         </div>
-                        <div className="notification-timestamp">{notification.timestamp}</div>
+                        <div className="notification-timestamp">{calculateTimeDifference(notification.timestamp)}</div>
                     </div>
                 ))}
             </div>
